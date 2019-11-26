ec91ad3f346c065201bc6ba81662461332a80765
cobbzac <cobbzac@urbandaleschools.com>
2019-11-26 14:20:10 -0600

    still need to to ascii

--- 6.0_Jedi_Training.py
+++ 6.0_Jedi_Training.py
@@ -3,19 +3,23 @@ Sign your name:________________
 
 
 1.) Convert Binary 101010 to Hexadecimal.
+15
 
 2.) Convert Binary 11101001 to Octal.
+4007
 
 3.) Convert Hexadecimal FC to Binary.
+11001111
 
 4.) Convert Hexadecimal 1F to Decimal.
+241
 
 5.) Convert Hexadecimal #FAAFBD to RGB.
-
+r:175 g:250 b:235
 6.) Convert Octal 70 to Binary.
-
+111
 7.) Convert RGB (32,128,64) to Hexadecimal.
-
+#020804
 8.) Convert 01000001, 01010011, 01000011, 01001001, 01001001 to ASCII.
-
+A S
 '''
