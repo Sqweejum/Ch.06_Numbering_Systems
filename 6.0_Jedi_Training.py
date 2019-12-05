b9900ae9a30dc30a687d864e2dc2f296afc49580
cobbzac <cobbzac@urbandaleschools.com>
2019-12-05 13:46:09 -0600

    corrected

--- 6.0_Jedi_Training.py
+++ 6.0_Jedi_Training.py
@@ -10,16 +10,16 @@ Sign your name:________________
 
 3.) Convert Hexadecimal FC to Binary.
 
-00111111
+11110000
 4.) Convert Hexadecimal 1F to Decimal.
 31
 
 5.) Convert Hexadecimal #FAAFBD to RGB.
-r:175 g:250 b:235
+r:250 g:175 b:189
 6.) Convert Octal 70 to Binary.
-111
+111000
 7.) Convert RGB (32,128,64) to Hexadecimal.
-#020804
+#208040
 8.) Convert 01000001, 01010011, 01000011, 01001001, 01001001 to ASCII.
 A, s, c, i, i
 '''
